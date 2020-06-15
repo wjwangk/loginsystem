bf22e2042ebe0559a5097904e2782ae64f6109ef
majun <majun01@fshows.com>
2020-06-15 23:47:31 +0800

    修改目录

--- src/main/java/com/xiaowenzi/example/demo/LoginSystemApplication.java
+++ src/main/java/com/xiaowenzi/example/demo/LoginSystemApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class LoginSystemApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(LoginSystemApplication.class, args);
     }
 
 }
